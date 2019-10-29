d64a0131a424bc36e74f1262b4e673ab84927740
takuya yamaguchi <takuyayamaguchi@takuyanoMacBook-Pro.local>
2019-10-29 20:20:51 +0900

    image position change

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '19cefe7a3557d4c2296ac3b34a3afc494c1deb250cccf493cda2cfafed43c7ced7665e79b99b528227aa51045bff79fc7081c08ccc26e60d3fa0779d9685a856'
